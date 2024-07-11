c1f16ba96ab60b4513441e7b12845ed65c946668
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2024-07-11 14:01:13 +0200

    REST api v0.2

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,9 +1,38 @@
 
-import { DiaryEntry } from '../types'
+import { DiaryEntry, NewDiaryEntry, NonSensitiveInfoDiaryEntry } from '../types'
 import diaryData from './diaries.json'
 
-const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
 
-export const getEntries = () => diaries
+export const getEntries = (): DiaryEntry[] => diaries
 
-export const addEntry = () => null
+export const findById = (id:number): NonSensitiveInfoDiaryEntry | undefined => {
+    const entry = diaries.find(d => d.id === id)
+    if (entry != null) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { comment, ...restOfDiary } = entry
+        return restOfDiary
+    }
+    return undefined
+}
+
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
+    return diaries.map(({ id, date, weather, visibility }) => {
+            return {
+                id,
+                date,
+                weather,
+                visibility
+            }  
+    })
+}
+
+export const addDiary = (newDiaryEntry: NewDiaryEntry): DiaryEntry => {
+    const newDiary = {
+        id: Math.max(...diaries.map(d => d.id)) + 1,
+        ...newDiaryEntry
+    }
+
+    diaries.push(newDiary)
+    return newDiary
+}
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,14 @@
-export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
-export type Visibility = 'great' | 'good' | 'ok' | 'poor'
+import { Weather, Visibility} from './enums'
 
 export interface DiaryEntry {
-    id: number,
-    date: string,
-    weather: Weather,
-    visibility: Visibility,
+    id: number
+    date: string
+    weather: Weather
+    visibility: Visibility
     comment: string
 }
+
+// export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, 'id' | 'date' | 'weather' | 'visibility'>
+
+export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
+export type NewDiaryEntry = Omit<DiaryEntry, 'id'>
